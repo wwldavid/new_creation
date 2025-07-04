93b004eb9336adc1215785d1e987f7f7ae0d5659
davidwanlongwu <davidwu.web@gmail.com>
2025-07-03 18:29:07 -0600

    install bcryptjs, uninstall bcrypt, fix No native build was found for platform=linux arch=x64 runtime=node abi=127 uv=1 libc=glibc node=2

--- src/app/api/applications/route.js
+++ src/app/api/applications/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import prisma from "../../../../lib/prisma";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 
 export const runtime = "nodejs";
 
